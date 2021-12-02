6d6e8bdf4b5954479de4dbe312126b5cdcbaa09b
Alex Dudnyk <dudniksasha@gmail.com>
2021-12-02 16:51:48 +0200

    files

---
+++ Classes.cs
@@ -0,0 +1,82 @@
+using System;
+using System.Linq;
+using System.Security.Claims;
+using System.Text.Encodings.Web;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+
+namespace WebApplication1
+{
+    public class ApiKeyAuthNOptions : AuthenticationSchemeOptions
+    {
+        public string ApiKeyValue { get; set; }
+
+        public string KeyParameterOrHeaderName { get; set; }
+    }
+
+    public static class ApiKeyAuthNDefaults
+    {
+        public const string SchemaName = "ApiKey";
+        public const string KeyName = "X-Api-Key";
+    }
+
+    public class ApiKeyAuthN : AuthenticationHandler<ApiKeyAuthNOptions>
+    {
+        public ApiKeyAuthN(
+            IOptionsMonitor<ApiKeyAuthNOptions> options,
+            ILoggerFactory logger,
+            UrlEncoder encoder,
+            ISystemClock clock)
+            : base(options, logger, encoder, clock)
+        {
+        }
+
+        protected override Task<AuthenticateResult> HandleAuthenticateAsync()
+        {
+            var apiKey = ParseApiKey(); // handles parsing QueryString
+
+            if (string.IsNullOrEmpty(apiKey)) //no key was provided - return NoResult
+                return Task.FromResult(AuthenticateResult.NoResult());
+
+            if (string.Compare(apiKey, Options.ApiKeyValue, StringComparison.Ordinal) == 0)
+            {
+                var principal = BuildPrincipal(Scheme.Name, Options.ApiKeyValue, Options.ClaimsIssuer ?? "ApiKeyValue");
+
+                return Task.FromResult(AuthenticateResult.Success(new AuthenticationTicket(principal, Scheme.Name))); //Success. Key matched
+            }
+
+            return Task.FromResult(AuthenticateResult.Fail($"Invalid API Key provided.")); //Wrong key was provided
+        }
+
+        protected string ParseApiKey()
+        {
+            if (Request.Headers.TryGetValue(Options.KeyParameterOrHeaderName, out var value))
+                return value.FirstOrDefault();
+
+            if (Request.Query.TryGetValue(Options.KeyParameterOrHeaderName, out value))
+                return value.FirstOrDefault();
+
+            return string.Empty;
+        }
+
+        static ClaimsPrincipal BuildPrincipal(
+            string schemeName,
+            string name,
+            string issuer,
+            params Claim[] claims)
+        {
+            var identity = new ClaimsIdentity(schemeName);
+
+            identity.AddClaim(new Claim(ClaimTypes.NameIdentifier, name, ClaimValueTypes.String, issuer));
+            identity.AddClaim(new Claim(ClaimTypes.Name, name, ClaimValueTypes.String, issuer));
+            identity.AddClaim(new Claim(ClaimTypes.Role, "admin", ClaimValueTypes.String, issuer));
+
+            identity.AddClaims(claims);
+
+            var principal = new ClaimsPrincipal(identity);
+            return principal;
+        }
+    }
+}
---
+++ Startup.cs
@@ -0,0 +1,88 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.OpenApi.Models;
+using Microsoft.AspNetCore.Mvc.Authorization;
+
+namespace WebApplication1
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddAuthentication(ApiKeyAuthNDefaults.SchemaName)
+                .AddScheme<ApiKeyAuthNOptions, ApiKeyAuthN>(ApiKeyAuthNDefaults.SchemaName, opt =>
+                {
+                    opt.ApiKeyValue = "Hello-World";
+                    opt.KeyParameterOrHeaderName = ApiKeyAuthNDefaults.KeyName;
+                    opt.ClaimsIssuer = "API-Issuer";
+                });
+
+            services.AddAuthorization();
+
+            services.AddControllers(o => o.Filters.Add(new AuthorizeFilter()));
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "WebApplication1", Version = "v1" });
+                c.AddSecurityDefinition(ApiKeyAuthNDefaults.SchemaName, new OpenApiSecurityScheme
+                {
+                    Description = "Api key needed to access the endpoints. X-Api-Key: My_API_Key",
+                    In = ParameterLocation.Header,
+                    Name = ApiKeyAuthNDefaults.KeyName,
+                    Type = SecuritySchemeType.ApiKey
+                });
+
+                c.AddSecurityRequirement(new OpenApiSecurityRequirement
+                {
+                    {
+                        new OpenApiSecurityScheme
+                        {
+                            Name = ApiKeyAuthNDefaults.SchemaName,
+                            Type = SecuritySchemeType.ApiKey,
+                            In = ParameterLocation.Header,
+                            Reference = new OpenApiReference
+                            {
+                                Type = ReferenceType.SecurityScheme,
+                                Id = ApiKeyAuthNDefaults.SchemaName
+                            },
+                        },
+                        new string[] {}
+                    }
+                });
+            });
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+                app.UseSwagger();
+                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "WebApplication1 v1"));
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+
+            app.UseAuthentication();
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
